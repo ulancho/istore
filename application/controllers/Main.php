6bd3e2eaf044e39a08da0904c094541d96c311de
ulancho <ulan.five@gmail.com>
2021-08-30 22:52:37 +0600

    add new page

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,11 +2,31 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('pagination');
+		$this->load->helper('url');
+		$this->load->library('email');
+		$this->load->helper('html');
+//		$this->load->model('AdminModels');
+//		$this->load->database();
+		header('Content-Type: text/html; charset=utf-8');
+	}
+
 	/*Стр: Главная*/
 	public function index()
 	{
 		$this->load->view('site/header');
 		$this->load->view('site/index');
         $this->load->view('site/footer');
+	}
+
+	/*Стр: О нас*/
+	public function about()
+	{
+		$this->load->view('site/header');
+		$this->load->view('site/about');
+		$this->load->view('site/footer');
 	}
 }
---
+++ application/views/site/about.php
@@ -0,0 +1,166 @@
+<div class="wrapper">
+	<section class="about-us-first">
+		<div class="h-100 container d-flex flex-wrap">
+			<div class="text-center w-100 my-5">
+				<h3 class="sf-sem main-title mb-5" data-aos="fade-zoom-in" data-aos-easing="ease-in-back"
+					data-aos-delay="10" data-aos-offset="0" style="line-height: 100%;">
+					iStore - фирменный магазин Apple <br/> в Бишкеке</h3>
+				<img class="response-img about_us_image" src="media/about_us_page/2020.20.26-00015_r9b2z4I.jpg"
+					 alt="" data-aos="fade-down" data-aos-offset="100" data-aos-duration="1500">
+			</div>
+		</div>
+	</section>
+	<section class="about-us-second">
+		<div class="h-100 container">
+			<div class="d-flex flex-wrap justify-content-center  mt-5">
+				<div class="col-lg-6 col-12">
+					<h5 class="sf-medium secondary-title mb-5 text-left" data-aos="fade-zoom-in"
+						data-aos-delay="10" data-aos-offset="0">
+						У нас можно приобрести всю линейку продукций компании Apple по самым привлекательным
+						ценам
+					</h5>
+					<p>С 2013 года мы знакомим бишкекчан с полной линейкой продукции Apple, а также предлагаем
+						большой выбор дополнительных устройств, гаджетов и аксессуаров. В магазине вы можете
+						протестировать iMac, MacBook, iPad или iPhone, все девайсы в открытом доступе.
+						<br/><br/>Наши опытные консультанты ответят на все вопросы и помогут определиться с
+						выбором, а главное &mdash; подберут то, что подойдет именно вам.</p>
+					<p>Кроме того, мы предлагаем нашим клиентам полное гарантийное обслуживание, неограниченное
+						время сервисного обслуживания, бонусы по накопительной программе, выгодные предложения и
+						акции!<br/>⠀<br/>Всегда рады видеть вас!</p>
+				</div>
+				<div class="col-lg-6 col-12 pr-0" data-aos="fade-zoom-in" data-aos-delay="10"
+					 data-aos-offset="0">
+					<div class="d-flex flex-wrap justify-content-center">
+						<div class="col-lg-6 col-12 px-2 mt-3" data-aos="fade-zoom-in" data-aos-delay="10"
+							 data-aos-offset="0">
+							<div class="card about-card">
+								<div class="d-flex">
+									<img src="media/about_us_page/headphones_support_1.png" alt="">
+									<p class="sf-medium small-title ml-2">Консультация и сервис</p>
+								</div>
+								<p class="sf-light short-desc">
+									Профессиональный и приветливый персонал; подбор и предложение только
+									оптимальных решений; персональный менеджер
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-6 col-12 px-2 mt-3" data-aos="fade-zoom-in" data-aos-delay="10"
+							 data-aos-offset="0">
+							<div class="card about-card">
+								<div class="d-flex">
+									<img src="media/about_us_page/Layer_1.png" alt="">
+									<p class="sf-medium small-title ml-2">Цены и расчеты</p>
+								</div>
+								<p class="sf-light short-desc">
+									Любая форма оплаты: наличный и безналичный расчет, банковские карты Visa,
+									MasterCard; возможность приобрести продукцию в кредит
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-6 col-12 px-2 mt-3" data-aos="fade-zoom-in" data-aos-delay="10"
+							 data-aos-offset="0">
+							<div class="card about-card">
+								<div class="d-flex">
+									<img src="media/about_us_page/Wrench_1.png" alt="">
+									<p class="sf-medium small-title ml-2">Постпродажный сервис</p>
+								</div>
+								<p class="sf-light short-desc">
+									Консультации по использованию; гарантийное обслуживание
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-6 col-12 px-2 mt-3" data-aos="fade-zoom-in" data-aos-delay="10"
+							 data-aos-offset="0">
+							<div class="card about-card">
+								<div class="d-flex">
+									<img src="media/about_us_page/Box_4.png" alt="">
+									<p class="sf-medium small-title ml-2">Всегда в наличии</p>
+								</div>
+								<p class="sf-light short-desc">
+									Айфоны, Айпады, Макбуки, а также оригинальные аксессуары для Ваших гаджетов
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="d-flex flex-wrap justify-content-center my-5">
+				<div class="col-12 text-center" data-aos="fade-zoom-in" data-aos-delay="10" data-aos-offset="0">
+					<h5 class="sf-medium secondary-title mb-5 text-center">
+						Обратная связь
+					</h5>
+				</div>
+				<div class="col-lg-5 col-12 text-center w-100 about-us-map" data-aos="fade-zoom-in"
+					 data-aos-delay="10" data-aos-offset="0">
+					<a class="dg-widget-link"
+					   href="http://2gis.kg/bishkek/firm/70000001020196124/center/74.58800554275514,42.876247147981104/zoom/16?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap">Посмотреть
+						на карте Бишкека</a>
+					<div class="dg-widget-link"><a
+							href="http://2gis.kg/bishkek/firm/70000001020196124/photos/70000001020196124/center/74.58800554275514,42.876247147981104/zoom/17?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=photos">Фотографии
+							компании</a></div>
+					<div class="dg-widget-link"><a
+							href="http://2gis.kg/bishkek/center/74.588009,42.875669/zoom/16/routeTab/rsType/bus/to/74.588009,42.875669╎iStore, магазин?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">Найти
+							проезд до iStore, магазин</a></div>
+					<script charset="utf-8" src="https://widgets.2gis.com/js/DGWidgetLoader.js"></script>
+					<script charset="utf-8">new DGWidgetLoader({
+                            "width": 640,
+                            "height": 600,
+                            "borderColor": "#a3a3a3",
+                            "pos": {"lat": 42.876247147981104, "lon": 74.58800554275514, "zoom": 16},
+                            "opt": {"city": "bishkek"},
+                            "org": [{"id": "70000001020196124"}]
+                        });</script>
+					<noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует
+						JavaScript.
+						Включите его в настройках вашего браузера.
+					</noscript>
+				</div>
+				<div class="col-lg-5 col-12 px-2 " data-aos="fade-zoom-in" data-aos-delay="10"
+					 data-aos-offset="0">
+					<div class="px-2 h-100 d-flex flex-wrap bg-grey px-4 pb-5">
+						<div class="col-xl-6 col-12 pt-5">
+							<div class="d-flex">
+								<img src="static/img/phone.svg" alt="">
+								<p class="sf-medium callback-title ml-2">Наши номера</p>
+							</div>
+							<p class="sf-light callback-desc">
+								+996 (555) 802 000 <br> +996 (505) 802 000
+							</p>
+						</div>
+						<div class="col-xl-6 col-12 pt-5">
+							<div class="d-flex">
+								<img src="static/img/time-five.svg" alt="">
+								<p class="sf-medium  callback-title ml-2">График работы</p>
+							</div>
+							<p class="sf-light callback-desc">
+								Пн - Вс 10:00 - 20:00
+							</p>
+						</div>
+						<div class="col-xl-6 col-12 pt-5">
+							<div class="d-flex">
+								<img src="static/img/mail.svg" alt="">
+								<p class="sf-medium  callback-title ml-2">Наша почта</p>
+							</div>
+							<p class="sf-light callback-desc">
+								<a href="mailto: istorekg@gmail.com" class="sf-light">
+									istorekg@gmail.com
+								</a>
+							</p>
+						</div>
+						<div class="col-xl-6 col-12 pt-5">
+							<div class="d-flex">
+								<img src="static/img/pin.svg" alt="">
+								<p class="sf-medium callback-title ml-2">Адрес</p>
+							</div>
+							<p class="sf-light callback-desc">
+								пр. Манаса, 40 (пер. ул. Киевская)
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+</div>
